98d64cbf4bcc53cc54372c0bad0dee51096b0f14
bata24 <tooooooooooooooooooooohsaka@gmail.com>
2025-04-22 21:36:17 +0900

    fix load-file

--- gef.py
+++ gef.py
@@ -30092,22 +30092,55 @@ class LoadFileCommand(GenericCommand):
     _category_ = "03-f. Memory - Dump/Load"
 
     parser = argparse.ArgumentParser(prog=_cmdline_)
-    parser.add_argument("filepath", metavar="FILEPATH", help="the filepath to load.")
     parser.add_argument("location", metavar="LOCATION", type=AddressUtil.parse_address,
                         help="the memory address to load.")
+    parser.add_argument("file_path", metavar="FILE_PATH", help="the filepath to load.")
+    parser.add_argument("file_offset", metavar="FILE_OFFSET", nargs="?", default=0, type=lambda x: int(x, 0),
+                        help="the offset of the file to load.")
+    parser.add_argument("load_size", metavar="LOAD_SIZE", nargs="?", type=lambda x: int(x, 0),
+                        help="the size of the data to load.")
     _syntax_ = parser.format_help()
+
+    _note_ = [
+        "+-mmap_start--+",
+        "|             |             +-file_start--+",
+        "|             |             | ^           |",
+        "|             |             | |           |",
+        "|             |             | v           |",
+        "| LOCATION <----------------- FILE_OFFSET |",
+        "| ...         | ^           | ...         |",
+        "|             | | LOAD_SIZE |             |",
+        "| ...         | v           | ...         |",
+        "| end <---------------------- end         |",
+        "|             |             |             |",
+        "|             |             |             |",
+        "|             |             +-file_end----+",
+        "|             |",
+        "+-mmap_end----+",
+    ]
+    _note_ = "\n".join(_note_)
 
     @parse_args
     @only_if_gdb_running
     @exclude_specific_gdb_mode(mode=("qemu-system", "kgdb", "vmware"))
     def do_invoke(self, args):
-        if not os.path.exists(args.filepath):
-            err("Not found {:s}".format(args.filepath))
+        if not os.path.exists(args.file_path):
+            err("Not found {:s}".format(args.file_path))
             return
 
-        data_size = os.path.getsize(args.filepath)
-        if data_size == 0:
-            err("Unsupported zero size mapping")
+        if args.load_size is None:
+            data_size = os.path.getsize(args.file_path)
+            if data_size == 0:
+                err("Unsupported zero size mapping")
+                return
+        elif args.load_size < 0:
+            err("Invalid LOAD_SIZE")
+            return
+        else:
+            data_size = args.load_size
+
+        if args.file_offset < 0:
+            err("Invalid FILE_OFFSET")
             return
 
         # +-mmap_start--+               ^             ^
@@ -30128,6 +30161,7 @@ class LoadFileCommand(GenericCommand):
         mmap_end = AddressUtil.align_address_to_size(data_end, gef_getpagesize())
         mmap_size = mmap_end - mmap_start
 
+        # mmap
         res = gdb.execute("mmap {:#x} {:#x}".format(mmap_start, mmap_size), to_string=True)
         if "[!]" in res:
             err("Failed to mmap")
@@ -30140,15 +30174,20 @@ class LoadFileCommand(GenericCommand):
             err("Failed to mmap")
             return
 
-        fd = open(args.filepath, "rb")
-        offset = 0
-        while True:
-            data = fd.read(0x1000)
+        # read file and write to memory
+        fd = open(args.file_path, "rb")
+        if args.file_offset > 0:
+            fd.seek(args.file_offset, 0)
+
+        pos = data_start
+        remain_size = data_size
+        while remain_size > 0:
+            data = fd.read(min(0x1000, remain_size))
             if len(data) == 0:
                 break
-            write_memory(data_start + offset, data)
-            offset += len(data)
-
+            write_memory(pos, data)
+            pos += len(data)
+            remain_size -= len(data)
         return
 
 
