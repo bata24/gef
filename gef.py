5402c3c6571f868d2726711e2c8a6641f45f33ce
Andrey Konovalov <andreyknvl@gmail.com>
2026-01-26 00:12:19 +0100

    fix: allow v2p and p2v with kgdb

--- gef.py
+++ gef.py
@@ -95101,9 +95101,14 @@ class Virt2PhysCommand(GenericCommand):
 
     @parse_args
     @only_if_gdb_running
-    @only_if_specific_gdb_mode(mode=("qemu-system", "vmware"))
+    @only_if_specific_gdb_mode(mode=("qemu-system", "vmware", "kgdb"))
     @only_if_specific_arch(arch=("x86_32", "x86_64", "ARM32", "ARM64"))
     def do_invoke(self, args):
+        # This command depends on pagewalk, which requires access to system registers.
+        if is_kgdb() and not kgdb_has_system_registers():
+            err("Unsupported in kgdb mode without access to system registers")
+            return
+
         FORCE_PREFIX_S = None
         if is_arm32() or is_arm64():
             if args.force_normal:
@@ -95147,9 +95152,14 @@ class Phys2VirtCommand(GenericCommand):
 
     @parse_args
     @only_if_gdb_running
-    @only_if_specific_gdb_mode(mode=("qemu-system", "vmware"))
+    @only_if_specific_gdb_mode(mode=("qemu-system", "vmware", "kgdb"))
     @only_if_specific_arch(arch=("x86_32", "x86_64", "ARM32", "ARM64"))
     def do_invoke(self, args):
+        # This command depends on pagewalk, which requires access to system registers.
+        if is_kgdb() and not kgdb_has_system_registers():
+            err("Unsupported in kgdb mode without access to system registers")
+            return
+
         FORCE_PREFIX_S = None
         if is_arm32() or is_arm64():
             if args.force_normal:
